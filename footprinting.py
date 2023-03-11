ab3c6b24831b506d66ed2d355947a1b5674e7820
Julen Iñigo <julen.inigo@ikasle.egibide.org>
2023-03-11 18:11:24 +0100

    Ignorada la carpeta resultados y ahora puedes descargar archivos

--- footprinting.py
+++ footprinting.py
@@ -11,18 +11,25 @@ import requests
 from bs4 import BeautifulSoup
 
 def descargar_fichero(url, nombre_archivo):
+    # Comprobar si existe la carpeta footprinting
+    if not os.path.exists("resultados/footprinting"):
+        os.makedirs("resultados/footprinting")
+
+    ruta_archivo = os.path.join("resultados/footprinting",nombre_archivo)
+
     # Consulta para descargar el archivo
     respuesta = requests.get(url)
 
     # Guardar el contenido en un archivo
-    with open(nombre_archivo,'wb') as fichero:
+    with open(ruta_archivo,'wb') as fichero:
         fichero.write(respuesta.content)
 
     # Visualizar el contenido del fichero
 
-    with open(nombre_archivo,'r') as fichero:
+    with open(ruta_archivo,'r') as fichero:
         contenido = fichero.read()
         print(contenido)
+        print(colored("El contenido del archivo se ha guardado en: "+ ruta_archivo ,"green"))
 
 def footprinting():
     # Preguntar por la url
