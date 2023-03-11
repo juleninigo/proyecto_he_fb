a99954041a3e8f23b6356808cfe89bcd3501e5f8
Julen Iñigo <julen.inigo@ikasle.egibide.org>
2023-03-11 19:24:06 +0100

    Webscraper que busca exploits sin acabar

--- metasploit.py
+++ metasploit.py
@@ -1,7 +1,35 @@
 # Archivo para la funcion de busqueda de exploits
+import requests
+from bs4 import BeautifulSoup
+from termcolor import colored
 
 def buscarexploits():
-    print("search")
+    exploits = input(colored("¿Sobre que servicio quieres que busque los exploits? "))
+    url = "https://www.infosecmatter.com/metasploit-module-library/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    links = []
+    for td in soup.find_all('td', {'class': 'column-1'}):
+        for a in td.find_all('a'):
+            link = a.get("text")
+            if link and exploits in link:
+                links.append(link)
+
+    print(colored("Exploits encontrados: ","red"))
+    for enlace in links:
+        print(enlace)
+
 
 def buscarpayloads():
-    print("payloads")
+    payloads = input(colored("¿Sobre que servicio quieres que busque los exploits? "))
+    url = "https://www.infosecmatter.com/metasploit-module-library/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    links = []
+    for td in soup.find_all('td', {'class': 'column-1'}):
+        for a in td.find_all('a'):
+            link = a.get('href')
+            if link and payloads in link:
+                links.append(link)
