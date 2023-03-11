88ce22e38f6cd7df2e5854c6f15f60bed5f80352
Julen Iñigo <julen.inigo@ikasle.egibide.org>
2023-03-11 18:30:51 +0100

    Codigo comentado en caso de que de errores el path de python

--- fingerprinting.py
+++ fingerprinting.py
@@ -21,3 +21,31 @@ def fingerprinting():
         for puerto, estado in scanner[host]['tcp'].items():
             if estado['state'] == 'open':
                 print('  Puerto:', puerto, '- Estado:', estado['state'])
+
+
+
+## En caso de error con las librerias hacer lo siguiente
+'''
+try:
+    output = subprocess.check_output(['pip', 'show', 'python-nmap'])
+    output_lines = output.decode().split('\n')
+    for line in output_lines:
+        if line.startswith('Location:'):
+            nmap_path = line.split(': ')[1]
+            break
+except:
+    print('Error: No se pudo obtener la ruta de instalación de python-nmap')
+    exit()
+
+# Agregar la ruta a la variable PYTHONPATH
+try:
+    python_path = os.environ.get('PYTHONPATH', '')
+    nmap_path = os.path.join(nmap_path, 'nmap')
+    if python_path:
+        python_path += ';'
+    python_path += nmap_path
+    os.environ['PYTHONPATH'] = python_path
+    print('La ruta de python-nmap se agregó correctamente a la variable de entorno PYTHONPATH')
+except:
+    print('Error: No se pudo agregar la ruta de python-nmap a la variable de entorno PYTHONPATH')
+'''
